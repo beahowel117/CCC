fa8ecf03309343190c9009afc80a6581de894c13
Beth Howell <beahowel117@gmail.com>
2021-09-07 11:34:07 -0400

    added images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,123 +1,46 @@
-
+import React from 'react';
+import data from './data';
 
 function App() {
   return (
     <div className="grid-container">
     <header className="row">
       <div>
-        <a className="brand" href="index.html">CCC</a>
+        <a className="brand" href="/">CCC</a>
       </div>
       <div>
-        <a href="cart.html">Cart</a>
-        <a href="signin.html">Sign In</a>
+        <a href="/cart">Cart</a>
+        <a href="/signin">Sign In</a>
       </div>
     </header>
     <main>
-      <div className="row center">
-        <div className="card">
-          <a href="product.html">
+      <div>
+        <div className="row center">
+          {
+            data.products.map((product) => (
+              <div key ={product._id} className="card">
+              <a href={`/product/${product._id}`}>
+                <img className="medium" src={product.image} alt={product.name} />
+              </a>
+              <div className="card-body">
+              <a href={`/product/${product._id}`}>
+                  <h2>{product.name}</h2>
+                </a>
+                <div className="rating">
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                </div>
+                <div className="price">
+                  ${product.price}
+                </div>
+              </div>
+            </div>
+            ))
+          }
 
-            <img className="medium" src="./images/p1.jpg" alt="product" />
-          </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>Drunk Elephant Face Cream</h2>
-            </a>
-            <div className="rating">
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-            </div>
-            <div className="price">
-              $80
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <a href="product.html">
-
-            <img className="medium" src="./images/p1.jpg" alt="product" />
-          </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>Drunk Elephant Face Cream</h2>
-            </a>
-            <div className="rating">
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-            </div>
-            <div className="price">
-              $80
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <a href="product.html">
-
-            <img className="medium" src="./images/p1.jpg" alt="product" />
-          </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>Drunk Elephant Face Cream</h2>
-            </a>
-            <div className="rating">
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-            </div>
-            <div className="price">
-              $80
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <a href="product.html">
-
-            <img className="medium" src="./images/p1.jpg" alt="product" />
-          </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>Drunk Elephant Face Cream</h2>
-            </a>
-            <div className="rating">
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star-o"></i> </span>
-              <span> <i className="fa fa-star-half-o"></i> </span>
-            </div>
-            <div className="price">
-              $80
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <a href="product.html">
-
-            <img className="medium" src="./images/p1.jpg" alt="product" />
-          </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>Drunk Elephant Face Cream</h2>
-            </a>
-            <div className="rating">
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-              <span> <i className="fa fa-star"></i> </span>
-            </div>
-            <div className="price">
-              $80
-            </div>
-          </div>
         </div>
       </div>
     </main>
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -46,3 +46,5 @@ const data = {
     }
   ]
 }
+
+export default data; 
