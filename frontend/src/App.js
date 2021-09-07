504d2fec9f2c02f433f55a537f1e16588458d7b3
Beth Howell <beahowel117@gmail.com>
2021-09-07 11:09:30 -0400

    added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,129 @@
+
+
+function App() {
+  return (
+    <div className="grid-container">
+    <header className="row">
+      <div>
+        <a className="brand" href="index.html">CCC</a>
+      </div>
+      <div>
+        <a href="cart.html">Cart</a>
+        <a href="signin.html">Sign In</a>
+      </div>
+    </header>
+    <main>
+      <div className="row center">
+        <div className="card">
+          <a href="product.html">
+
+            <img className="medium" src="./images/p1.jpg" alt="product" />
+          </a>
+          <div className="card-body">
+            <a href="product.html">
+              <h2>Drunk Elephant Face Cream</h2>
+            </a>
+            <div className="rating">
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+            </div>
+            <div className="price">
+              $80
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <a href="product.html">
+
+            <img className="medium" src="./images/p1.jpg" alt="product" />
+          </a>
+          <div className="card-body">
+            <a href="product.html">
+              <h2>Drunk Elephant Face Cream</h2>
+            </a>
+            <div className="rating">
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+            </div>
+            <div className="price">
+              $80
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <a href="product.html">
+
+            <img className="medium" src="./images/p1.jpg" alt="product" />
+          </a>
+          <div className="card-body">
+            <a href="product.html">
+              <h2>Drunk Elephant Face Cream</h2>
+            </a>
+            <div className="rating">
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+            </div>
+            <div className="price">
+              $80
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <a href="product.html">
+
+            <img className="medium" src="./images/p1.jpg" alt="product" />
+          </a>
+          <div className="card-body">
+            <a href="product.html">
+              <h2>Drunk Elephant Face Cream</h2>
+            </a>
+            <div className="rating">
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star-o"></i> </span>
+              <span> <i className="fa fa-star-half-o"></i> </span>
+            </div>
+            <div className="price">
+              $80
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <a href="product.html">
+
+            <img className="medium" src="./images/p1.jpg" alt="product" />
+          </a>
+          <div className="card-body">
+            <a href="product.html">
+              <h2>Drunk Elephant Face Cream</h2>
+            </a>
+            <div className="rating">
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+              <span> <i className="fa fa-star"></i> </span>
+            </div>
+            <div className="price">
+              $80
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer className="row center"> All Rights Reserved</footer>
+  </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,48 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Drunk Elephant Face Cream',
+      category: 'skin care',
+      image: '/images/p1.jpg',
+      price: 60,
+      brand: 'Drunk Elephant',
+      rating: 5,
+      numReviews: 10,
+      description: 'A rescue cream infused with six African oils and a plant ceramide complex for intense moisture and replenishing barrier support.'
+    },
+    {
+      _id: '2',
+      name: 'Farmacy Honey Cream',
+      category: 'skin care',
+      image: '/images/p2.jpg',
+      price: 38,
+      brand: 'Farmacy',
+      rating: 5,
+      numReviews: 10,
+      description: 'A warming honey face mask infused with essential B vitamins and antioxidants to hydrate, smooth, and visibly renew the skin, leaving a glowing look.'
+    },
+    {
+      _id: '3',
+      name: 'Glossier Dew Serum',
+      category: 'skin care',
+      image: '/images/p3.jpg',
+      price: 24,
+      brand: 'Glossier',
+      rating: 5,
+      numReviews: 10,
+      description: 'A shortcut to the way your skin looks after a full skincare routine—dewy, glowing, cared-for—in one long-wearing product.'
+    },
+    {
+      _id: '4',
+      name: 'Ren Face Wash',
+      category: 'skin care',
+      image: '/images/p4.jpg',
+      price: 34,
+      brand: 'Ren',
+      rating: 5,
+      numReviews: 10,
+      description: 'A gel facial wash for sensitive skin, developed to leave skin cleansed and comfortable with no dryness.'
+    }
+  ]
+}
