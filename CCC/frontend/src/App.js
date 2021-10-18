b8a390f097a8677ec1d34d15bb53d13e950999e7
Beth Howell <beahowel117@gmail.com>
2021-10-18 18:16:07 -0400

    created home and product screens

--- CCC/frontend/src/App.js
+++ CCC/frontend/src/App.js
@@ -1,34 +1,29 @@
 import React from 'react';
-import data from './data';
-import Product from './components/Product';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
- 
-  return (
-    <div className="grid-container">
-    <header className="row">
-      <div>
-        <a className="brand" href="/">CCC</a>
-      </div>
-      <div>
-        <a href="/cart">Cart</a>
-        <a href="/signin">Sign In</a>
-      </div>
-    </header>
-    <main>
-      <div>
-        <div className="row center">
-          {
-            data.products.map((product) => (
-              <Product key={product._id} product={product} />
-            ))
-          }
 
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">CCC</a>
         </div>
-      </div>
-    </main>
-    <footer className="row center"> All Rights Reserved</footer>
-  </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <Route path="/product/:id" component={ProductScreen}></Route>
+        <Route path="/" component={HomeScreen} exact></Route>
+      </main>
+      <footer className="row center"> All Rights Reserved</footer>
+    </div>
+  </BrowserRouter>
   );
 }
 
